c9bdae8bf091caebc4f322f513e94473d4899f93
Reck Lee <ReckLee@dualred.onmicrosoft.com>
2023-01-06 15:25:11 +0800

    add Header Master and Sub

---
+++ src/components/Commons/HeaderMaster.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="my-6 border-2 border-stone-400 text-center py-4 rounded-3xl text-gray-600 w-5/6 mx-auto">
+        <h2 class="text-4xl font-extrabold"><slot></slot></h2>
+    </div>
+</template>
---
+++ src/components/Commons/HeaderSub.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="border-l-8 border-l-blue-600 mb-4 mt-10 pl-4 text-stone-700">
+        <h3 class="text-3xl"><slot></slot></h3>
+    </div>
+</template>
